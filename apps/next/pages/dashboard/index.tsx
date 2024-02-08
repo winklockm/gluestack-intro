2d9c2895628336907fe4cfac3b922a8b10a20a61
Maggie Whitlock <mwhitlock@f3wireless.com>
2024-02-08 11:39:28 -0600

    add dashboard

---
+++ apps/next/pages/dashboard/index.tsx
@@ -0,0 +1,6 @@
+import Dashboard from '@gluestack/app/Dashboard/Dashboard'
+
+const Screen = () => {
+  return <Dashboard />
+}
+export default Screen
---
+++ packages/app/screens/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import {
+    Text,
+    Button
+} from '@gluestack-ui/themed'
+
+export default function Dashboard() {
+    return (
+        <>
+            <Text>Hello, World!</Text>
+            <Button>Click Me</Button>
+        </>
+    )
+}
